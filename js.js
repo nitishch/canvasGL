d897fbf0268e5e51e455617a9d27ba2f8d9a9b13
nitish <nitishchandrachinta@gmail.com>
2014-10-11 22:25:13 +0530

    Just included some files with no functionality whatsoever

---
+++ js.js
@@ -0,0 +1,11 @@
+var noofPixels = 1;
+
+var height = screen.availHeight;
+var width = screen.availWidth;
+
+//document.getElementById("output").firstChild.nodeValue = "height: " + height + "\n width: " + width;
+
+var canvas = document.getElementsByTagName("canvas")[0];
+var context = canvas.getContext("2d");
+context.fillRect(10, 10, 100, 50);
+
