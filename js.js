dde504a8b973f0d2df10b7c238f4cd16fc93fc36
nitish <nitishchandrachinta@gmail.com>
2014-10-13 00:07:07 +0530

    Grid is also done. But the color looks ugly. Issue: When we go to pixel level, grid takes the entire space. But when calculating, forget the grid. Focus only on canvas.

--- js.js
+++ js.js
@@ -1,10 +1,7 @@
-var noofPixels = 30;
+var noofPixels = 50;
 //var totalHeight = screen.availHeight;
 //var totalWidth = screen.availWidth;
-
 var fixedDimension = 600;
-
-
 var width = 2*fixedDimension, height = fixedDimension;
 //document.getElementById("output").firstChild.nodeValue = "height: " + height + "\n width: " + width;
 
@@ -22,8 +19,23 @@ context.fillRect(0, 0, width, height);
 var xoff = canvas.getBoundingClientRect().left, yoff = canvas.getBoundingClientRect().top;
 /*--------------------Ends Initialisation------------------------------*/
 
-context.fillStyle = "#40bf45";
+/* These two for-loops draw the grid */
+for(var x = 0.5; x < width; x = x + noofPixels){
+	context.moveTo(x, 0);
+	context.lineTo(x, height);
+}
 
+for(var y = 0.5; y < height; y = y + noofPixels){
+	context.moveTo(0, y);
+	context.lineTo(width, y);
+}
+/* An advise is ignore looking at the grid. Forget it's there. Trust that it draws correctly*/
+
+context.strokeStyle = "#eee";
+context.stroke();
+
+
+context.fillStyle = "#40bf45";
 var colour = function(event){
 	var relativeX = event.pageX - xoff, relativeY = event.pageY - yoff; //these are the coordinates treating canvas left top as origin. We don't use canvas coordinates;
 	context.fillRect(noofPixels*Math.floor((relativeX-1)/noofPixels), noofPixels*Math.floor((relativeY - 1)/noofPixels), noofPixels, noofPixels);
@@ -31,3 +43,4 @@ var colour = function(event){
 }
 
 canvas.addEventListener("click", colour);
+
